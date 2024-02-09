6524fc87a6079412de73fbfccef3abc773ffb983
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-02-09 18:48:40 -0300

    new structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,1 @@
-export { default as Breadcrumb } from "./Breadcrumb";
-export { default as Button } from "./Button";
-export { default as InputNumber } from "./InputNumber";
-export { default as InputText } from "./InputText";
-export { default as ToggleSwitch } from "./ToggleSwitch";
+export * from "./atoms";
